149b6828c541a95ee4f6869c0b0557bc701cc8e8
msub2 <msub2official@gmail.com>
2021-02-15 17:26:10 -0500

    Test file retrieval

--- silk-brush/my-files/getfiles.js
+++ silk-brush/my-files/getfiles.js
@@ -1,42 +1,52 @@
 window.onload = () => {
-    console.log("test");
-
     let openReq = indexedDB.open("/idbfs").onsuccess = function (event) {
         let db = event.target.result;
         let objectStore = db.transaction("FILE_DATA").objectStore("FILE_DATA").openCursor().onsuccess = function (event) {
             let cursor = event.target.result;
             if (cursor) {
                 console.log(cursor.key);
-                console.log(cursor.key.substring(cursor.key.length - 4));
+                let extension = cursor.key.split(".").pop();
 
-                /*var glbFile = cursor.value.contents.buffer;
+                if (extension.length > 4) {
+                    cursor.continue(); // Not a valid extension, keep going
+                }
+                else {
+                    var file = cursor.value.contents.buffer;
 
-                // Get window.URL object
-                var URL = window.URL || window.webkitURL;
+                    // Get window.URL object
+                    var URL = window.URL || window.webkitURL;
 
-                // Create an invisible <a> element
-                var a = document.createElement("a");
-                a.style.display = "none";
-                document.body.appendChild(a);
+                    // Create an <a> element
+                    var a = document.createElement("a");
+                    a.innerHTML = cursor.key;
 
-                // Set the HREF to a Blob representation of the data to be downloaded
-                a.href = URL.createObjectURL(new Blob([glbFile], {type: "model/gltf-binary"}));
+                    // Set the HREF to a Blob representation of the data to be downloaded
+                    // Then add to appropriate <div>
+                    switch (extension) {
+                        case "tilt":
+                            a.href = URL.createObjectURL(new Blob([file], { type: "application/octet-stream" }));
+                            document.getElementById("sketch").appendChild(a);
+                            break;
+                        case "glb":
+                            a.href = URL.createObjectURL(new Blob([file], { type: "model/gltf-binary" }));
+                            document.getElementById("model").appendChild(a);
+                            break;
+                        case "png":
+                            a.href = URL.createObjectURL(new Blob([file], { type: "image/png" }));
+                            document.getElementById("snapshot").appendChild(a);
+                            break;
+                        default:
+                            break;
+                    }
 
-                // Use download attribute to set set desired file name
-                a.setAttribute("download", filename);
+                    // Use download attribute to set set desired file name
+                    a.setAttribute("download", filename);
 
-                // Trigger the download by simulating click
-                a.click();
-
-                // Cleanup
-                window.URL.revokeObjectURL(a.href);
-                document.body.removeChild(a);
-                */
-                cursor.continue();
+                    cursor.continue();
+                }
             }
             else {
-                console.log("Not found");
-                console.log(event);
+                console.log("Cursor has reached end of object store");
             }
         };
     };
