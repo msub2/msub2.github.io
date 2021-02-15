e0601a1995c406bda894d89f5f714f3d2e746cf2
msub2 <msub2official@gmail.com>
2021-02-15 15:53:44 -0500

    Test IDB cursor logging

--- silk-brush/my-files/getfiles.js
+++ silk-brush/my-files/getfiles.js
@@ -1,3 +1,42 @@
 window.onload = () => {
     console.log("test");
+
+    let openReq = indexedDB.open("/idbfs").onsuccess = function (event) {
+        let db = event.target.result;
+        let objectStore = db.transaction("FILE_DATA").objectStore("FILE_DATA").openCursor().onsuccess = function (event) {
+            let cursor = event.target.result;
+            if (cursor) {
+                console.log(cursor);
+
+                /*var glbFile = cursor.value.contents.buffer;
+
+                // Get window.URL object
+                var URL = window.URL || window.webkitURL;
+
+                // Create an invisible <a> element
+                var a = document.createElement("a");
+                a.style.display = "none";
+                document.body.appendChild(a);
+
+                // Set the HREF to a Blob representation of the data to be downloaded
+                a.href = URL.createObjectURL(new Blob([glbFile], {type: "model/gltf-binary"}));
+
+                // Use download attribute to set set desired file name
+                a.setAttribute("download", filename);
+
+                // Trigger the download by simulating click
+                a.click();
+
+                // Cleanup
+                window.URL.revokeObjectURL(a.href);
+                document.body.removeChild(a);
+                */
+                cursor.continue();
+            }
+            else {
+                console.log("Not found");
+                console.log(event);
+            }
+        };
+    };
 }
